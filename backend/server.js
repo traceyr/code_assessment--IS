58a34c8044eab76e737e3a5b76505c3f769b3398
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-09-19 19:11:34 -0700

    added basic server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({  extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
